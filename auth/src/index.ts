16770e7660a86a7be006f14e44f0cce9c1702fec
Joshua Jones <JJones@internal.cubase.org>
2021-06-03 16:33:26 -0400

    add jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,29 +1,55 @@
 import express from 'express';
 import 'express-async-errors';
-import {json} from 'body-parser';
+import { json } from 'body-parser';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
-import {currentUserRouter} from './routes/currentuser';
-import {signinRouter} from './routes/signin';
-import {signupRouter} from './routes/signup';
-import {signoutRouter} from './routes/signout';
-import {errorHandler} from './middlewares/errorhandler';
-import {NotFoundError} from './errors/notfounderror';
+import { currentUserRouter } from './routes/currentuser';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/errorhandler';
+import { NotFoundError } from './errors/notfounderror';
 
 const app = express();
+app.set('trust proxy', true);
 
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
 
-app.all('*',async()=>{
-    throw new NotFoundError();
+app.all('*', async () => {
+  throw new NotFoundError();
 });
 
 app.use(errorHandler);
 
-app.listen(3000,()=>{
+const start = async () => {
+  if (!process.env.JWTKEY) {
+    throw new Error('JWTKEY must be defined');
+  }
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+  console.log('Connected to MongoDB');
+  app.listen(3000, () => {
     console.log('Listening on port 3000!');
-});
+  });
+};
+
+start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,35 +1,53 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-import {RequestValidationError} from '../errors/requestvalidationerror';
-import {DatabaseConnectionError} from '../errors/databaseconnectionerror';
+import { User } from '../models/user';
+import { RequestValidationError } from '../errors/requestvalidationerror';
+import { BadRequestError } from '../errors/badrequesterror';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.post('/api/users/signup',[
-  
-  body('email')
-    .isEmail()
-    .withMessage('Invalid email'),
-  body('password')
-    .trim()
-    .isLength({min: 4, max: 20})
-    .withMessage('Password must be between 4 and 20 characters')
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-  ],(req:Request,res:Response)=>{
-  
-  const errors = validationResult(req);
-  
-  if(!errors.isEmpty()){
-    throw new RequestValidationError(errors.array());
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError('Email in use');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    //Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWTKEY!
+    );
+
+    //Store it on session object
+    req.session = { jwt: userJwt };
+
+    res.status(201).send(user);
   }
-  
-  const {email, password} = req.body;
-  
-  console.log('Creating a user');
+);
 
-  throw new DatabaseConnectionError();
-  
-  res.send({});
-});
-
-export {router as signupRouter}
+export { router as signupRouter };
