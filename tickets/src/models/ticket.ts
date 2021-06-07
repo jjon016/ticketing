75f648bf89c75baa35246b1f9df52bd0db1e072e
Joshua Jones <JJones@internal.cubase.org>
2021-06-07 16:57:59 -0400

    ticket added

---
+++ tickets/src/models/ticket.ts
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+
+//An interface that describes the properties
+// required to create a new Ticket
+interface TicketAttrs {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+//An interface that describes the properties
+// that a Ticket model has
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+//An interface that describes the properties
+// that a Ticket document has
+interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+const TicketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.__v;
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+TicketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', TicketSchema);
+
+export { Ticket };
---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,61 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../app';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): string[];
+    }
+  }
+}
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWTKEY = 'asdfasdf';
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  //Build a JWT payload {id,email,iat}
+  const payload = {
+    id: '123lksjdf',
+    email: 'test@test.com',
+  };
+
+  //create the JWT
+  const token = jwt.sign(payload, process.env.JWTKEY!);
+
+  //Build session object {jwt: MY_JWT}
+  const session = { jwt: token };
+
+  //Turn session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  //Take JSON and encode it to base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  //return a string thats a cookie
+  return [`express:sess=${base64}`];
+};
